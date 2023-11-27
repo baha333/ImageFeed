f37d4c894711e5632ba61c7a91a88d6b0b8d707f
baha333 <adamovbaha@gmail.com>
2023-11-27 16:30:09 +0300

    сверстан Launch screen и главный экран приложения с 20 фотографиями

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Bakhadir on 26.11.2023.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+}
