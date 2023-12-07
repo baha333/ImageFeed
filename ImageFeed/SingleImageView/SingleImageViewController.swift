58ea30c59a38d269ded8480437e78cee18ddb588
baha333 <adamovbaha@gmail.com>
2023-12-07 13:19:26 +0300

    Создал SingleImage

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Bakhadir on 07.12.2023.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+}
